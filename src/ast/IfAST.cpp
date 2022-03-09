e97968309d5ed124bbd2d65422d4602fa57d2533
Greyson Murray <greyson.murray@gmail.com>
2022-03-09 15:05:59 -0500

    implement copy constructor

--- src/ast/IfAST.cpp
+++ src/ast/IfAST.cpp
@@ -10,7 +10,9 @@ IfAST::IfAST(std::unique_ptr<AST> expr, std::unique_ptr<CompoundAST> body, std::
       other(std::move(other)) {}
 
 IfAST::IfAST(const IfAST& other)
-    : expr(std::unique_ptr<AST>(other.expr->clone())) {}
+    : expr(std::unique_ptr<AST>(other.expr->clone())),
+      body(std::unique_ptr<CompoundAST>(dynamic_cast<CompoundAST*>(other.body->clone()))),
+      other(std::unique_ptr<IfAST>(dynamic_cast<IfAST*>(other.other->clone()))) {}
 
 llvm::Value* IfAST::accept(CodegenVisitor& cg) 
 {
