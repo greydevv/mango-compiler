6b865085a4642716fb28c486edb3967053fd55a8
Greyson Murray <greyson.murray@gmail.com>
2022-03-15 17:49:32 -0400

    implement ForAST class

---
+++ src/ast/ForAST.cpp
@@ -0,0 +1,30 @@
+#include <memory>
+#include "ForAST.h"
+#include "CompoundAST.h"
+#include "../visitors/CodegenVisitor.h"
+#include "../visitors/ASTStringifier.h"
+#include "llvm/IR/Value.h"
+
+ForAST::ForAST(std::unique_ptr<VariableAST> var, std::unique_ptr<ExpressionAST> expr, std::unique_ptr<ExpressionAST> stepExpr, std::unique_ptr<CompoundAST> body)
+    : var(std::move(var)), expr(std::move(expr)), stepExpr(std::move(stepExpr)), body(std::move(body)) {}
+
+ForAST::ForAST(const ForAST& other)
+    : var(),
+      expr(std::unique_ptr<ExpressionAST>(dynamic_cast<ExpressionAST*>(other.expr->clone()))),
+      stepExpr(std::unique_ptr<ExpressionAST>(dynamic_cast<ExpressionAST*>(other.stepExpr->clone()))), 
+      body(std::unique_ptr<CompoundAST>(dynamic_cast<CompoundAST*>(other.body->clone()))) {}
+
+llvm::Value* ForAST::accept(CodegenVisitor& cg) 
+{
+    return cg.codegen(this);
+}
+
+std::string ForAST::accept(ASTStringifier& sf, int tabs) 
+{
+    return sf.toString(this, tabs);
+}
+
+ForAST* ForAST::cloneImpl()
+{
+    return new ForAST(*this);
+}
---
+++ src/ast/ForAST.h
@@ -0,0 +1,26 @@
+#ifndef FOR_AST_H
+#define FOR_AST_H
+
+#include <memory>
+#include "AST.h"
+#include "ExpressionAST.h"
+#include "../visitors/CodegenVisitor.h"
+#include "../visitors/ASTStringifier.h"
+
+class ForAST : public AST
+{
+    public:
+        std::unique_ptr<VariableAST> var;
+        std::unique_ptr<ExpressionAST> expr;
+        std::unique_ptr<ExpressionAST> stepExpr;
+        std::unique_ptr<CompoundAST> body;
+
+        ForAST(std::unique_ptr<VariableAST> var, std::unique_ptr<ExpressionAST> expr, std::unique_ptr<ExpressionAST> stepExpr, std::unique_ptr<CompoundAST> body);
+        ForAST(const ForAST& other);
+        virtual llvm::Value* accept(CodegenVisitor& cg) override;
+        virtual std::string accept(ASTStringifier& sf, int tabs = 0) override;
+    protected:
+        virtual ForAST* cloneImpl() override;
+};
+
+#endif
