74ee200313a79a6e26f3ef1c793c0d7785812287
Greyson Murray <greyson.murray@gmail.com>
2022-04-05 16:44:09 -0400

    Remove main.cpp from gitignore

---
+++ src/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Exception.h"
+#include "compile.h"
+#include "checks.h"
+#include "io.h"
+
+int main(int argc, char const *argv[])
+{
+    CompileArgs args = parseArgs(std::vector<std::string>(argv+1, argc+argv));
+    if (args.fnames.size() == 0)
+    {
+        fmt::print(fmt::emphasis::bold | fmt::fg(fmt::color::orange_red), "Error");
+        fmt::print(fmt::emphasis::bold, ": no file name(s) specified\n");
+        return 1;
+    }
+
+    if (args.debug)
+        // validate enums
+        runChecks();
+
+    for (auto& fname : args.fnames)
+    {
+        if (fname.find_first_of('-') == 0)
+        {
+            fmt::print(fmt::emphasis::bold | fmt::fg(fmt::color::orange_red), "Error");
+            fmt::print(fmt::emphasis::bold, ": unknown argument: '{}'\n", fname);
+            return 1;
+        }
+
+        try
+        {
+            std::ostringstream outs;
+
+            // the '[weakly_]canonical' methods resolve things like '../' and
+            // './' in the given paths
+            // for example, a/b/c/../d is resolved to the literal a/b/d
+            //
+            // using 'weakly_canonical' does not throw an error if the file is
+            // not found
+            std::filesystem::path absPath = std::filesystem::weakly_canonical(fname);
+            // std::cout << std::filesystem::relative(std::filesystem::absolute(fname));
+            std::shared_ptr<ModuleAST> prog = compile(absPath, outs);
+
+            if (args.debug)
+            {
+                prog->print();
+                std::cout << '\n' << outs.str();
+            }
+        }
+        catch (const BaseException& e)
+        {
+            std::cout << e.what();
+            return 1;
+        }
+
+    }
+
+    return 0;
+}
