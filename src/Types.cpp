112306e2424af41c2e091715121aad2b5270637b
greydevv <greysonmurray.dev@gmail.com>
2022-05-15 20:23:01 -0400

    Add boolean type

--- src/Types.cpp
+++ src/Types.cpp
@@ -12,10 +12,34 @@ Type typeFromString(const std::string& str)
         return Type::eVoid;
     else if (str == "int")
         return Type::eInt;
+    else if (str == "bool")
+        return Type::eBool;
     else if (str == "array")
         return Type::eArray;
     else
         return Type::eUnd;
+}
+
+bool isNumeric(Type t)
+{
+    // return 'true' if type is considered numeric, otherwise false
+    switch (t)
+    {
+        case Type::eInt:
+        case Type::eBool:
+            return true;
+        default:
+            return false;
+    }
+}
+
+bool typeCompat(Type a, Type b)
+{
+    // check if type 'a' is compatible with type 'b' for weak-typing
+    // right now, only numeric types are compatible with eachother
+    if (isNumeric(a) && isNumeric(b))
+        return true;
+    return false;
 }
 
 bool isArrayType(Type t)
