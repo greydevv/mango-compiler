60e956d872efcb7245f2c12840ed8b3fc30cc9a4
greydevv <greysonmurray.dev@gmail.com>
2022-06-11 11:37:11 -0400

    Implement prefix/postfix type in unary expressions

--- src/ast/UnaryExprAST.cpp
+++ src/ast/UnaryExprAST.cpp
@@ -5,11 +5,31 @@
 #include "../visitors/ASTStringifier.h"
 #include "llvm/IR/Value.h"
 
-UnaryExprAST::UnaryExprAST(std::unique_ptr<AST> operand, Operator::op_type op)
-    : operand(std::move(operand)), op(op) {}
+UnaryExprAST::UnaryExprAST(std::unique_ptr<AST> operand, Operator::op_type op, unary_type type)
+    : operand(std::move(operand)), op(op), type(type) {}
 
 UnaryExprAST::UnaryExprAST(const UnaryExprAST& other)
     : operand(std::unique_ptr<AST>(other.operand->clone())), op(other.op) {}
+
+bool UnaryExprAST::isPrefix()
+{
+    return type == UNARY_PRE;
+}
+
+bool UnaryExprAST::isPostfix()
+{
+    return type == UNARY_POST;
+}
+
+std::unique_ptr<UnaryExprAST> UnaryExprAST::unaryPrefix(std::unique_ptr<AST> operand, Operator::op_type op)
+{
+    return std::make_unique<UnaryExprAST>(std::move(operand), op, UNARY_PRE);
+}
+
+std::unique_ptr<UnaryExprAST> UnaryExprAST::unaryPostfix(std::unique_ptr<AST> operand, Operator::op_type op)
+{
+    return std::make_unique<UnaryExprAST>(std::move(operand), op, UNARY_POST);
+}
 
 llvm::Value* UnaryExprAST::accept(ASTCodegenner& cg) 
 {
--- src/ast/UnaryExprAST.h
+++ src/ast/UnaryExprAST.h
@@ -11,16 +11,26 @@
 class UnaryExprAST : public AST
 {
     public:
+        enum unary_type {
+            UNARY_PRE,
+            UNARY_POST,
+        };
         std::unique_ptr<AST> operand;
         Operator::op_type op;
 
-        UnaryExprAST(std::unique_ptr<AST> operand, Operator::op_type op);
+        UnaryExprAST(std::unique_ptr<AST> operand, Operator::op_type op, unary_type type);
         UnaryExprAST(const UnaryExprAST& other);
+        bool isPrefix();
+        bool isPostfix();
+        static std::unique_ptr<UnaryExprAST> unaryPrefix(std::unique_ptr<AST> operand, Operator::op_type op);
+        static std::unique_ptr<UnaryExprAST> unaryPostfix(std::unique_ptr<AST> operand, Operator::op_type op);
         virtual Type accept(ASTValidator& vd) override;
         virtual llvm::Value* accept(ASTCodegenner& cg) override;
         virtual std::string accept(ASTStringifier& sf, int tabs = 0) override;
     protected:
         virtual UnaryExprAST* cloneImpl() override;
+    private:
+        unary_type type;
 };
 
 #endif
