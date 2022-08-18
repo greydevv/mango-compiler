b6d342139dce3574745e73242f4ae050969e5757
greydevv <greysonmurray.dev@gmail.com>
2022-08-17 21:18:03 -0400

    Location and assignment expression refactors

--- src/ast/ExpressionAST.cpp
+++ src/ast/ExpressionAST.cpp
@@ -8,16 +8,46 @@
 #include "../visitors/ASTStringifier.h"
 #include "llvm/IR/Value.h"
 
-ExpressionAST::ExpressionAST(std::unique_ptr<AST> LHS, std::unique_ptr<AST> RHS, Operator::op_type op)
-    : LHS(std::move(LHS)), RHS(std::move(RHS)), op(op) {}
+ExpressionAST::ExpressionAST(std::unique_ptr<AST> LHS, std::unique_ptr<AST> RHS, Operator::op_type op, SourceLocation opLoc)
+    : AST(LHS->loc), op(op), opLoc(opLoc), LHS(std::move(LHS)), RHS(std::move(RHS))
+{
+    calculateLoc();
+}
 
 ExpressionAST::ExpressionAST(std::unique_ptr<AST> LHS)
-    : LHS(std::move(LHS)), RHS(nullptr), op(Operator::OP_NOP) {}
+    : AST(LHS->loc), op(Operator::OP_NOP), opLoc(SourceLocation(0,0,0)), LHS(std::move(LHS)), RHS(nullptr)
+{
+    calculateLoc();
+}
+
+std::shared_ptr<AST> ExpressionAST::getLhs()
+{
+    return LHS;
+}
+
+std::shared_ptr<AST> ExpressionAST::getRhs()
+{
+    return RHS;
+}
+
+void ExpressionAST::setRhs(std::unique_ptr<AST> rhs)
+{
+    this->RHS = std::move(rhs);
+    calculateLoc();
+}
 
 ExpressionAST::ExpressionAST(const ExpressionAST& other)
-    : LHS(std::unique_ptr<AST>(other.LHS->clone())),
-      RHS(other.RHS ? std::unique_ptr<AST>(other.RHS->clone()) : nullptr),
-      op(other.op) {}
+    : AST(other.loc),
+      op(other.op),
+      opLoc(other.opLoc),
+      LHS(std::unique_ptr<AST>(other.LHS->clone())),
+      RHS(other.RHS ? std::unique_ptr<AST>(other.RHS->clone()) : nullptr) {}
+
+bool ExpressionAST::isAssignable()
+{
+    // can assign if no operation is taking place (i.e. no operator and no right-hand side)
+    return op == Operator::OP_NOP && !RHS && LHS->isAssignable();
+}
 
 llvm::Value* ExpressionAST::accept(ASTCodegenner& cg)
 {
@@ -37,4 +67,10 @@ std::string ExpressionAST::accept(ASTStringifier& sf, int tabs)
 ExpressionAST* ExpressionAST::cloneImpl()
 {
     return new ExpressionAST(*this);
+}
+
+void ExpressionAST::calculateLoc()
+{
+    if (RHS)
+      loc.len = (RHS->loc.x + RHS->loc.len) - loc.x;
 }
